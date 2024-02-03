d54f5f09a8d797bdf9206cdadebd6ec345e7a65e
|thenishant| <thenishant3@gmail.com>
2024-02-03 23:12:03 +0530

    Closing the modal

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -15,51 +15,40 @@ function ManageExpense({route, navigation}) {
     }, [navigation, isEditing]);
 
     function deleteExpenseHandler() {
-
+        navigation.goBack()
     }
 
     function cancelHandler() {
+        navigation.goBack()
     }
 
     function confirmHandler() {
+        navigation.goBack()
     }
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.buttons}>
-                <Button mode={'flat'} onPress={cancelHandler} style={styles.button}>Cancel</Button>
-                <Button onPress={confirmHandler} style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
-            </View>
-            {isEditing && (
-                <View style={styles.deleteContainer}>
-                    <IconButton icon={'trash'} color={GlobalStyles.colors.error500} size={36} onPress={() => {
-                    }}
-                    />
-                </View>
-            )
-            }
+    return (<View style={styles.container}>
+        <View style={styles.buttons}>
+            <Button mode={'flat'} onPress={cancelHandler} style={styles.button}>Cancel</Button>
+            <Button onPress={confirmHandler} style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
         </View>
-    )
+        {isEditing && (<View style={styles.deleteContainer}>
+            <IconButton icon={'trash'} color={GlobalStyles.colors.error500} size={36} onPress={() => {
+            }}
+            />
+        </View>)}
+    </View>)
 }
 
 export default ManageExpense
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 24,
-        backgroundColor: GlobalStyles.colors.gray500
-    },
-    buttons: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: "center"
-    },
-    button: {
-        minWidth: 120,
-        marginHorizontal: 8
-    },
-    deleteContainer: {
+        flex: 1, padding: 24, backgroundColor: GlobalStyles.colors.gray500
+    }, buttons: {
+        flexDirection: 'row', justifyContent: 'center', alignItems: "center"
+    }, button: {
+        minWidth: 120, marginHorizontal: 8
+    }, deleteContainer: {
         marginTop: 16,
         paddingTop: 8,
         borderTopWidth: 2,
