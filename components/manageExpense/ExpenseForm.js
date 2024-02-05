b762fc75b0e64614491231933b3c10985fb93486
|thenishant| <thenishant3@gmail.com>
2024-02-05 13:56:09 +0530

    Refactor ExpenseForm

--- components/manageExpense/ExpenseForm.js
+++ components/manageExpense/ExpenseForm.js
@@ -1,8 +1,22 @@
 import {StyleSheet, Text, View} from "react-native";
 import Input from "./Input";
+import {useState} from "react";
+import Button from "../UI/Button";
 
-function ExpenseForm() {
-    function amountChangeHandler() {
+function ExpenseForm({onCancel, onSubmit,submitButonLabel: submitButtonLabel}) {
+    const [inputValues, setInputValues] = useState({
+        amount: '',
+        date: '',
+        desc: ''
+    })
+
+    function inputChangeHandler(inputIdentifier, enteredValue) {
+        setInputValues((currentInput) => {
+            return {...currentInput, [inputIdentifier]: enteredValue}
+        })
+    }
+
+    function submitHandler() {
 
     }
 
@@ -12,18 +26,25 @@ function ExpenseForm() {
             <View style={styles.inputsRow}>
                 <Input style={styles.rowInput} label={"Amount"} textInputConfig={{
                     keyboardType: 'decimal-pad',
-                    onChangeText: amountChangeHandler
+                    onChangeText: inputChangeHandler.bind(this, 'amount'),
+                    value: inputValues.amount
                 }}/>
                 <Input style={styles.rowInput} label={"Date"} textInputConfig={{
                     placeholder: 'YYYY-MM-DD',
                     maxLength: 10,
-                    onChangeText: () => {
-                    }
+                    onChangeText: inputChangeHandler.bind(this, 'date'),
+                    value: inputValues.date
                 }}/>
             </View>
             <Input label={"Description"} textInputConfig={{
-                multiline: true
+                multiline: true,
+                onChangeText: inputChangeHandler.bind(this, 'desc'),
+                value: inputValues.desc
             }}/>
+            <View style={styles.buttons}>
+                <Button mode={'flat'} onPress={onCancel} style={styles.button}>Cancel</Button>
+                <Button onPress={submitHandler} style={styles.button}>{submitButtonLabel}</Button>
+            </View>
         </View>
     )
 }
@@ -38,13 +59,17 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20,
         color: 'black',
-        marginVertical:24,
-        textAlign:'center'
+        marginVertical: 24,
+        textAlign: 'center'
     },
     inputsRow: {
         flexDirection: "row",
         justifyContent: "space-between"
     }, rowInput: {
         flex: 1
-    }
+    }, buttons: {
+        flexDirection: 'row', justifyContent: 'center', alignItems: "center"
+    }, button: {
+        minWidth: 120, marginHorizontal: 8
+    },
 });
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -40,11 +40,7 @@ function ManageExpense({route, navigation}) {
     }
 
     return (<View style={styles.container}>
-        <ExpenseForm/>
-        <View style={styles.buttons}>
-            <Button mode={'flat'} onPress={cancelHandler} style={styles.button}>Cancel</Button>
-            <Button onPress={confirmHandler} style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
-        </View>
+        <ExpenseForm onCancel={cancelHandler} submitButonLabel={isEditing ? 'Update' : 'Add'}/>
         {isEditing && (<View style={styles.deleteContainer}>
             <IconButton icon={'trash'} color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler}
             />
@@ -57,10 +53,6 @@ export default ManageExpense
 const styles = StyleSheet.create({
     container: {
         flex: 1, padding: 24, backgroundColor: GlobalStyles.colors.gray500
-    }, buttons: {
-        flexDirection: 'row', justifyContent: 'center', alignItems: "center"
-    }, button: {
-        minWidth: 120, marginHorizontal: 8
     }, deleteContainer: {
         marginTop: 16,
         paddingTop: 8,
