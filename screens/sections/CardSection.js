b4390aa2a888be838168c3c82b75c8ebf3e02db5
|thenishant| <thenishant3@gmail.com>
2024-03-31 19:44:19 +0530

    Change the api to display expenses

--- screens/sections/CardSection.js
+++ screens/sections/CardSection.js
@@ -7,18 +7,29 @@ import ExpensePerMonthChart from "../charts/ExpensePerMonthChart";
 import IncomeVsExpenseChart from "../charts/IncomeVsExpenseChart";
 import PaymentModePerMonth from "../charts/PaymentMode";
 import Card from "../../components/UI/Card";
+import ExpenseSummary from "../../components/expensesOutput/ExpenseSummary";
 
 function CardSection() {
     const [refreshing, setRefreshing] = useState(false);
-    const [transactionsInAMonth, setTransactionsInAMonth] = useState({
+    const initialFigures = {
         sumOfIncome: 0, sumOfExpense: 0, balance: 0
-    });
+    };
+    const [currentMonthTransactions, setCurrentMonthTransactions] = useState(initialFigures);
+    const [lastMonthTransaction, setLastMonthTransaction] = useState(initialFigures);
 
     const fetchData = async () => {
         try {
             const month = moment().format('MMM');
-            const transactionsInAMonthResponse = await axios.get(buildUrl(`${apiEndpoints.transactionsInAMonth}?month=${month}`));
-            setTransactionsInAMonth(transactionsInAMonthResponse.data);
+            const lastMonth = moment().subtract(1, 'months').format('MMM');
+
+            const monthlyTransactions = await axios.get(buildUrl(`${apiEndpoints.monthlyExpense}`));
+            const response = monthlyTransactions.data;
+
+            const lastMonthData = response[1];
+            const currentMonthData = response[0];
+
+            setCurrentMonthTransactions(currentMonthData);
+            setLastMonthTransaction(lastMonthData)
         } catch (error) {
             console.error(error);
         } finally {
@@ -40,17 +51,21 @@ function CardSection() {
         refreshControl={<RefreshControl
             refreshing={refreshing}
             onRefresh={onRefresh}
-        />}
-    >
-        <View style={styles.firstRow}>
-            <Card style={styles.expenseAmount} amount={transactionsInAMonth.sumOfExpense} heading={'Expenses'}/>
-            <Card style={styles.incomeAmount} amount={transactionsInAMonth.sumOfIncome} heading={'Income'}/>
-            <Card style={styles.balanceAmount} amount={transactionsInAMonth.balance} heading={'Balance'}/>
-        </View>
+        />}>
         <View>
-            <ExpensePerMonthChart/>
-            <IncomeVsExpenseChart/>
-            <PaymentModePerMonth/>
+            <View style={styles.expenseSummary}>
+                <ExpenseSummary expenses={[{amount: lastMonthTransaction.expense}]} periodName={'Last month'}/>
+            </View>
+            <View style={styles.firstRow}>
+                <Card style={styles.expenseAmount} amount={currentMonthTransactions.expense} heading={'Expenses'}/>
+                <Card style={styles.incomeAmount} amount={currentMonthTransactions.income} heading={'Income'}/>
+                <Card style={styles.balanceAmount} amount={currentMonthTransactions.balance} heading={'Balance'}/>
+            </View>
+            <View>
+                <ExpensePerMonthChart/>
+                <IncomeVsExpenseChart/>
+                <PaymentModePerMonth/>
+            </View>
         </View>
     </ScrollView>);
 }
@@ -59,14 +74,16 @@ export default CardSection;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, marginTop: -20, backgroundColor: '#eef4f8'
+        flex: 1, backgroundColor: '#eef4f8'
     }, firstRow: {
-        flexDirection: "row", justifyContent: "center",
+        flexDirection: "row", justifyContent: "center", paddingHorizontal: 12, marginTop: -20
     }, incomeAmount: {
         color: '#70e000'
     }, expenseAmount: {
         color: '#ef233c'
     }, balanceAmount: {
         color: '#00b4d8'
-    }
+    }, expenseSummary: {
+        padding: 24, backgroundColor: '#eef4f8', flex: 1
+    },
 });
