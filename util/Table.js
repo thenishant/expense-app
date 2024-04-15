07c3b342a3f219666c5e09bd4e4af7e4d99da335
|thenishant| <thenishant3@gmail.com>
2024-04-15 12:54:04 +0530

    Add convertToTableDataWithIcon()

--- util/Table.js
+++ util/Table.js
@@ -1,9 +1,21 @@
-const convertToTableData = (data) => {
-    const tableData = [];
-    for (let i = 0; i < data.length; i += 3) {
-        tableData.push(data.slice(i, i + 3));
+export function convertToTableDataWithIcon(categorySVGs) {
+    if (!Array.isArray(categorySVGs) || categorySVGs.length === 0 || !categorySVGs[0]) {
+        console.error('Invalid categorySVGs data:', categorySVGs);
+        return []
     }
-    return tableData;
-}
 
-export default convertToTableData
+    const tableData = [];
+    const categories = Object.keys(categorySVGs[0]);
+    categories.forEach(category => {
+        const svgFilename = categorySVGs[0][category];
+        const svgUri = (`.../assets/icons/${svgFilename}`);
+        tableData.push(`${svgUri} ${category}`);
+    });
+
+    const rowData = [];
+    for (let i = 0; i < tableData.length; i += 3) {
+        rowData.push(tableData.slice(i, i + 3));
+    }
+
+    return rowData;
+}
