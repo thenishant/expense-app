86752c5c6d91cdf5144b9c8de7fa132bbf6d9f57
|thenishant| <thenishant3@gmail.com>
2024-04-20 15:57:23 +0530

    Add the logic to show the loader when the data is being fetched

--- screens/sections/CardSection.js
+++ screens/sections/CardSection.js
@@ -4,11 +4,15 @@ import axios from "axios";
 import Card from "../../components/UI/Card";
 import {apiEndpoints, buildUrl} from "../../constansts/Endpoints";
 import {getMonth} from "../../util/Date";
+import LoadingOverlay from "../../components/UI/LoadingOverlay";
+import ErrorOverlay from "../../components/UI/ErrorOverlay";
 
 function CardSection({selectedMonth}) {
     const initialFigures = {
         income: 0, expense: 0, balance: 0
     };
+    const [isFetching, setIsFetching] = useState(true)
+    const [error, setError] = useState('')
     const [currentMonthTransactions, setCurrentMonthTransactions] = useState(initialFigures);
 
     useEffect(() => {
@@ -16,6 +20,7 @@ function CardSection({selectedMonth}) {
     }, [selectedMonth]);
 
     const fetchData = async () => {
+        setIsFetching(true)
         try {
             const month = getMonth(selectedMonth);
             const monthlyTransactions = await axios.get(buildUrl(`${apiEndpoints.monthlyExpense}`))
@@ -24,15 +29,23 @@ function CardSection({selectedMonth}) {
         } catch (error) {
             console.error(error);
         }
+        setIsFetching(false)
     };
 
-    return (<View
-        style={styles.container}>
-        <View style={styles.firstRow}>
+    useEffect(() => {
+        fetchData();
+    }, [selectedMonth]);
+
+    if (isFetching) return <LoadingOverlay/>
+
+    if (!isFetching && error) return <ErrorOverlay message={error}/>
+
+    return (<View style={styles.container}>
+        {currentMonthTransactions ? (<View style={styles.firstRow}>
             <Card style={styles.expenseAmount} amount={currentMonthTransactions.expense} heading={'Expenses'}/>
             <Card style={styles.incomeAmount} amount={currentMonthTransactions.income} heading={'Income'}/>
             <Card style={styles.balanceAmount} amount={currentMonthTransactions.balance} heading={'Balance'}/>
-        </View>
+        </View>) : (<ErrorOverlay message={`No expense details found for ${getMonth(selectedMonth)}`}/>)}
     </View>);
 }
 
