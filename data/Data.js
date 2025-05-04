d1f75b19693515b0873f556865acfa4b2b48f336
thenishant <thenishant3@gmail.com>
2025-05-04 22:24:16 +0530

    Update Data

--- data/Data.js
+++ data/Data.js
@@ -1,25 +1,24 @@
 export const typesData = ['Expense', 'Investment', 'Income'];
 export const paymentModeData = ["💳 Credit Card", "🏛 Bank Account", "💵 Cash"];
-export const incomeCategoryType = ['💼 Interest', '📈 ROI', '💼 Salary', '💳 Credit Exchange'];
+export const incomeCategoryType = ['💼 Interest', '📈 ROI', '💼 Salary', '💵 Reimbursement', '💳 Credit Exchange'];
 export const investmentCategoryType = ['📈 Stocks', '💹 Mutual Funds'];
 
 export const categories = {
-    '🏦 Loan': ['📑 Personal Loan', '🚗 Car Loan', '🏠 Mortgage', '🎓 Education Loan', '📊 Business Loan', '🔄 Debt Repayment'],
-    '🍺 Alcohol': ['🍺 Beer', '🍷 Wine', '🥃 Spirits', '🍾 Liquor', '🍹 Cocktails', '🍻 Bar Drinks'],
+    '🏦 Loan': ['📑 Personal', '🚗 Car', '🏠 Home', '🎓 Education', '📊 Business', '🔄 Debt Repayment'],
+    '🍺 Alcohol': ['🍺 Beer', '🍷 Wine', '🍾 Liquor', '🍹 Cocktails',],
     '🛍 Shopping': ['👚 Clothing', '💻 Electronics', '🧸 Toys', '💄 Beauty Products', '⚽ Sporting Goods'],
-    '🥗 Grocery': ['🥒 Vegetables', '🍉 Fruits', '🥡 Dairy', '📱 Online Grocery'],
-    // '🍔 Eatery': ['🍴 Cafeteria', '🍫 Snacks', '🍹 Beverages', '🌭 Street Food', '🍟 Fast Food'],
-    '🍽 Eating': ['🍽️ Restaurant', '🍻 Pub', '📱 Online order', '🍔 Fast Food', '🍹 Beverages', '☕ Café', '🍽️ Takeaway'],
-    '🏕 Leisure': ['🌍 Travel', '🎭 Entertainment', '🏞️ Outdoor Activities', '🎮 Gaming', '🎬 Movies', '🎮 Video Games'],
-    '🏠 Home': ['🏡 Utilities', '🛋️ Maid', '🔧 Appliances', '⚒️ Maintenance', '🏠 Home Improvement'],
-    '🚗 Travel': ['⛽️ Petrol', '🚖 Cab Booking', '🚗 Tolls', '🗒 Tickets'],
+    '🥗 Grocery': ['🥒 Vegetables', '🍉 Fruits', '🥡 Dairy', '📱 Online Grocery', '🍞 Bakery'],
+    '🍽 Eating': ['🍽️ Restaurant', '📱 Online', '🍹 Beverages', '🍽 Takeaway'],
+    '🏕 Leisure': ['🌍 Travel', '🎭 Entertainment', '🏞️ Activities', '🎮 Gaming', '🎬 Movies', '🍻 Restaurant'],
+    '🏠 Home': ['🏡 Utilities', '🛋️ Maid', '🔧 Items', '🏠 Home Improvement'],
+    '🚗 Travel': ['⛽ Petrol', '🚖 Cab', '🚗 Tolls', '🗒 Tickets'],
     '🎁 Gift': ['🎉 Birthday', '💑 Anniversary', '🎄 Holiday', '👰 Wedding', '🎁 Gift Cards'],
     '🏥 Insurance': ['💖 Health', '🩼 Life'],
     '🩺 Medical': ['🩺 Doctor Visits', '💊 Medicines', '🦷 Dental', '👓 Optical'],
     '📺 Bills': ['🌐 Internet', '💪 Gym', '⚡ Electricity', '🚰 Water', '📱 Mobile', '📺 Cable'],
     '🎓 Education': ['📚 Books', '🎓 Courses', '🛠️ Workshops', '🎓 Events'],
     '💆 Personal Care': ['💆 Body Care', '🛍️ Products'],
-    '👶 Family': ['👶 Baby', '👦 Toys', '👧 Clothing', '📚 Education', '👩 Wife']
+    '👶 Family': ['👶 Baby', '👩 Wife']
 };
 
 export const getMainCategories = () => {
@@ -27,6 +26,12 @@ export const getMainCategories = () => {
 };
 
 export const getSubCategories = (mainCategory) => {
+    if (!mainCategory) {
+        return [];
+    }
+    if (!categories[mainCategory]) {
+        return [];
+    }
     const category = categories[mainCategory];
     return category || null;
 };
