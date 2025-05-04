f230501be082ebf9005ef8f04f7b263a7fa66f52
thenishant <thenishant3@gmail.com>
2025-05-04 23:58:03 +0530

    Add new screen Investments.js

---
+++ screens/Investments.js
@@ -0,0 +1,23 @@
+import React, {useLayoutEffect} from "react";
+import {View, Text, StyleSheet} from "react-native";
+import {GlobalStyles} from "../constansts/styles";
+
+function Investments({route, navigation}) {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: 'Investments'
+        });
+    }, [navigation]);
+
+    return (<View>
+        <Text>Investments</Text>
+    </View>);
+}
+
+export default Investments;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, padding: 24, backgroundColor: GlobalStyles.colors.gray500
+    }
+});
