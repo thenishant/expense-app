6598f58471d79d655f6e3df5c4ed9ff3831e7b38
thenishant <thenishant3@gmail.com>
2025-05-09 17:52:57 +0530

    Removed color from ProgressBar.js

--- components/UI/ProgressBar.js
+++ components/UI/ProgressBar.js
@@ -1,37 +1,20 @@
 import React from "react";
 import * as Progress from 'react-native-progress';
-import {StyleSheet, Text, View} from "react-native";
-import {GlobalStyles} from "../../constansts/styles";
+import {StyleSheet, View} from "react-native";
 
 function ProgressBar({options}) {
-    const {progress = 0, width = 100, height = 10, color = "#3b5998"} = options;
-    const percentage = Math.round(progress * 100);
-
-    const textColor = progress > 0.3 ? GlobalStyles.colors.white500 : GlobalStyles.colors.black700;
-
-    return (<View style={[styles.container, {width, height}]}>
+    return (<View>
         <Progress.Bar
             style={styles.progressBar}
             {...options}
         />
-        <View style={styles.overlay}>
-            <Text style={[styles.percentageText, {color: textColor}]}>
-                {percentage}%
-            </Text>
-        </View>
-    </View>);
+    </View>)
 }
 
-export default ProgressBar;
+export default ProgressBar
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center', marginTop: 15
-    }, progressBar: {
-        position: 'absolute', zIndex: 0,
-    }, overlay: {
-        position: 'absolute', width: '100%', alignItems: 'center', zIndex: 1,
-    }, percentageText: {
-        fontSize: 15, fontWeight: 'bold',
+    progressBar: {
+        marginTop: 15,
     },
 });
